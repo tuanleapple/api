16ece342b25ecead65c92545b3951e21b1a2c0aa
TuanLK <tuanlk@ethan-tech.com>
2023-12-03 23:27:47 +0700

    last update

---
+++ database/migrations/2023_12_01_085008_print_history.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('print_histories', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('factory_number');
+            $table->timestamp('date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('print_histories');
+    }
+};
